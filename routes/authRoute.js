69651cc99123579962fd0a94ce9248e2c7ce6231
Anjali331997 <anjaliap3march1997@gmail.com>
2023-11-15 13:50:40 +0530

    admin functionality is updated for the authRoute page

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,9 +6,13 @@ const { authMiddleware, isAdmin } = require('../middleware/authMiddleware')
 // router.post('/resgister');
 authRouter.post('/signup', createUser);
 authRouter.post('/login', loginUser);
-authRouter.get('/allUsers', getallUser)
+
+//Only admin can access these endpoints
+//we are pasing a middleware here 'authMiddleware' with will check if the user  logged in or not
+//the next middlewar isAdmin will check if the logged in persion is an asmin or not
+authRouter.get('/allUsers',authMiddleware,isAdmin, getallUser)
 authRouter.get('/:id', authMiddleware, isAdmin, getaSingleUser)
-authRouter.put('/edit/:id', editUSer)
-authRouter.delete('/:id', deleteUSer)
+authRouter.put('/edit/:id',authMiddleware,isAdmin, editUSer)
+authRouter.delete('/:id', authMiddleware,isAdmin, deleteUSer)
 
 module.exports = { authRouter };
